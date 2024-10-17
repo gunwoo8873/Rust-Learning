3041767377edd03525be39522689c27c85144ccc
choigunwoo <gunwoo8873@outlook.kr>
2024-10-17 21:23:05 +0900

    Update : for branche

--- src/bin/branches.rs
+++ src/bin/branches.rs
@@ -2,6 +2,8 @@ pub fn main() {
     if_branches();
     loop_branches();
     label_loop_branches();
+    while_branches();
+    for_branches();
 }
 
 fn if_branches() {
@@ -64,5 +66,41 @@ fn label_loop_branches() {
 
 /*
 다중 Loop 에서 remain 이 10에서 9로 반환값을 출력을 할 때 default_count 가 0에서 5에 도달 할 때 까지 반복을 한다.
-하지만 '의 사용처는 많아 단순히 특정 Loop 의 이용함이 아니라
+하지만 '의 사용처는 많아 단순히 특정 Loop 의 이용함이 아니라 문자열이나 타입 명시 할 경우에 사용하는 것 같다.
+*/
+
+fn while_branches() {
+    let mut default_count = 0;
+
+    while default_count < 5 {
+        default_count += 1;
+
+        if default_count == 5 {
+            break
+        }
+        println!("While End Result : {}", default_count);
+    }
+
+    let arr = ["A", "B", "C", "D", "E"];
+
+    while default_count < 5 {
+        println!("Array While End Result = {}", arr[default_count]); // arr[index] = name[index 0..5]
+        default_count += 1;
+    }
+}
+
+fn for_branches() {
+    let arr = [10, 20, 30, 40, 50];
+
+    for element in arr { // in arr = arr[index] bind
+        println!("{}", element);
+    }
+
+    for element in (1..5).rev() { // in (1..5) = 1 ~ 4
+        println!("{}", element);
+    }
+}
+
+/*
+for 은 컬렌션의 아이템에 대하여 임의의 코드를 수행시킬 수 있다.
 */
