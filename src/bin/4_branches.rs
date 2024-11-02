3992f92075fad8f601e32adf9f925245d6c877be
Choigunwoo <gunwoo8873@outlook.kr>
2024-11-02 18:40:41 +0900

    Update : Branches Comment

--- src/bin/4_branches.rs
+++ src/bin/4_branches.rs
@@ -31,9 +31,7 @@ fn loop_branches() {
 
     println!("Loop Result : {result}");
 }
-/*
-let 을 통한 Loop 는 default_count 가 0에서 10에 도달할 때 까지의 과정을 출력하지 않고 결과값인 10만 출력한다.
-*/
+// Note : let 을 통한 Loop 는 default_count 가 0에서 10에 도달할 때 까지의 과정을 출력하지 않고 결과값인 10만 출력한다.
 
 fn label_loop_branches() {
     let mut default_count = 0;
@@ -63,11 +61,8 @@ fn label_loop_branches() {
 
     println!("End Count = {default_count}");
 }
-
-/*
-다중 Loop 에서 remain 이 10에서 9로 반환값을 출력을 할 때 default_count 가 0에서 5에 도달 할 때 까지 반복을 한다.
-하지만 '의 사용처는 많아 단순히 특정 Loop 의 이용함이 아니라 문자열이나 타입 명시 할 경우에 사용하는 것 같다.
-*/
+// Note : 다중 Loop 에서 remain 이 10에서 9로 반환값을 출력을 할 때 default_count 가 0에서 5에 도달 할 때 까지 반복을 한다.
+//        하지만 '의 사용처는 많아 단순히 특정 Loop 의 이용함이 아니라 문자열이나 타입 명시 할 경우에 사용하는 것 같다.
 
 fn while_branches() {
     let mut default_count = 0;
@@ -100,7 +95,4 @@ fn for_branches() {
         println!("{}", element);
     }
 }
-
-/*
-for 은 컬렌션의 아이템에 대하여 임의의 코드를 수행시킬 수 있다.
-*/
+// Note : for 은 컬렌션의 아이템에 대하여 임의의 코드를 수행시킬 수 있다.
