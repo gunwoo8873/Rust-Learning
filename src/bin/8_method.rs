f8df43a7dc69e659adfb054abf9ab148112602b0
Choigunwoo <gunwoo8873@outlook.kr>
2024-11-04 16:19:36 +0900

    Update : Struct Field

--- src/bin/8_method.rs
+++ src/bin/8_method.rs
@@ -1,20 +1,30 @@
+#[derive(Debug)]
+struct User
+{
+    username: String,
+    email: String,
+    password: String,
+    active: bool,
+}
+
 fn main()
 {
-
+    instance();
 }
 
-struct UserData
-{
-    name: String,
-    email: String,
-    active: bool,
-    address: String,
-}
 
-impl UserData
+fn instance()
 {
-    fn new(&self) -> String
+    let user_field = User
     {
-        self.name
-    }
+        username: String::from("User"),
+        email: String::from("user@outlook.kr"),
+        password: String::from("********"),
+        active: true,
+    };
+
+    println!("User Name : {:?}", user_field.username);
+    println!("User Email : {:?}", user_field.email);
+    println!("User Password : {:?}", user_field.password);
+    println!("User Active : {:?}", user_field.active);
 }
